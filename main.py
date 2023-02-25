3a201c6a33951616322b45d8ef5f5e707f22eebf
Brian Omosa Omwenga <44875298+maldev3555@users.noreply.github.com>
2023-02-25 15:47:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Load the data from the Excel sheet into a pandas DataFrame
+df = pd.read_excel("rptsalesdtls.xls")
+
+# Display the first few rows of the DataFrame
+print(df.head())
+
