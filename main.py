65d35d9ab11b2308ffeee093c24dd1b44cb90ad8
blueintell <>
2023-02-26 14:52:27 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
-# Load the data from the Excel sheet into a pandas DataFrame
-df = pd.read_excel("rptsalesdtls.xls")
+# Load the data
+df = pd.read_excel("salesreport.xls")
 
-# Display the first few rows of the DataFrame
-print(df.head())
+# Clean the data
+df.drop_duplicates(inplace=True)
+df.dropna(inplace=True)
+
+# Create a new feature for total sales per product
+df["Total Sales"] = df["Qty"] * df["Gross Sales"]
+
+# Split the data into training and testing sets
+X = df[["Qty", "Gross Sales", "Total Sales"]]
+y = df["product code"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Scale the features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Fit a linear regression model
+reg = LinearRegression()
+reg.fit(X_train_scaled, y_train)
+
+# Evaluate the model
+train_score = reg.score(X_train_scaled, y_train)
+test_score = reg.score(X_test_scaled, y_test)
+print("Training R^2 score: {:.3f}".format(train_score))
+print("Testing R^2 score: {:.3f}".format(test_score))
 
